e8e2fa3c071548ad2e86cce97a84c62295dced49
NguyenLDHE176088 <107171443+NguyenLDHE176088@users.noreply.github.com>
2024-10-21 16:46:09 +0700

    feat: add order viewing related parts

---
+++ VERBUM_FE_SEP490/composables/useOrders.ts
@@ -0,0 +1,64 @@
+import type { Order } from '~/types/order'
+import { ref } from 'vue'
+import { useToast } from '~/components/ui/toast'
+
+const { toast } = useToast()
+
+export const useOrders = () => {
+  const isLoading = ref(false)
+  const orders = ref<Order[]>([])
+  const order = ref<Order | null>(null)
+
+  const getOrders = async () => {
+    isLoading.value = true
+    try {
+      const { data: ordersData } = await useAPI<Order[]>('/order/get-all', {
+        method: 'GET',
+        credentials: 'include'
+      })
+      if (!ordersData?.value || ordersData.value.length === 0) {
+        toast({
+          title: 'No orders found',
+          description: 'There are no orders available'
+        })
+        orders.value = []
+      } else {
+        orders.value = ordersData.value
+      }
+    } catch (error) {
+      console.error(error)
+    } finally {
+      isLoading.value = false
+    }
+  }
+
+  const getOrder = async (id: string) => {
+    isLoading.value = true
+    try {
+      const { data: orderData } = await useAPI<Order>(`/order/get/`, {
+        query: { id },
+        method: 'GET',
+        credentials: 'include'
+      })
+
+      if (!orderData?.value) {
+        toast({
+          title: 'No order found',
+          description: 'This order does not exist'
+        })
+        navigateTo('/orders')
+        order.value = null
+      } else {
+        order.value = orderData.value
+      }
+    } catch (error) {
+      console.error(error)
+    } finally {
+      isLoading.value = false
+    }
+  }
+
+
+
+  return { isLoading, orders, order, getOrders, getOrder }
+}
--- VERBUM_FE_SEP490/types/order.ts
+++ VERBUM_FE_SEP490/types/order.ts
@@ -1,5 +1,5 @@
 export interface Order {
-    id: number
+    id: string
     name: string
     status: string
     createAt: string
