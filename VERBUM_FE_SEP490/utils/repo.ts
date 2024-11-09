26ce41813dcbea3929e4956c457fc96944f27ff4
DnMakyr <107171443+NguyenLDHE176088@users.noreply.github.com>
2024-11-09 08:59:00 +0700

    feat: add repository utility

---
+++ VERBUM_FE_SEP490/utils/repo.ts
@@ -0,0 +1,24 @@
+import type { $Fetch, NitroFetchRequest } from 'nitropack'
+import type { Order } from '@/types/order'
+import type { Language } from '@/types/language'
+export const repo = <T>(fetch: $Fetch<T, NitroFetchRequest>) => ({
+  async getLanguages(): Promise<Language[]> {
+    return fetch<Language[]>(`/lang`)
+  },
+  async getOrders(page: number, pageSize: number): Promise<Order[]> {
+    return fetch<Order[]>(`/order/get-all?$skip=${(page - 1) * pageSize}&$top=${pageSize}`)
+  },
+  async searchOrders(value: string): Promise<Order[]> {
+    return fetch<Order[]>(`/order/get-all?$filter=contains(orderName, '${value}')`)
+  },
+  async updateOrder(order: Partial<Order>): Promise<Order | null> {
+    return fetch<Order | null>(`/order/update`, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(order)
+    })
+  }
+})
+
