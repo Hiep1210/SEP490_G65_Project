70fc22385137800d9f77532f385178fa43100b8f
DnMakyr <107171443+NguyenLDHE176088@users.noreply.github.com>
2024-11-11 23:14:52 +0700

    feat: add approve, reject and submit file

--- VERBUM_FE_SEP490/composables/useJobs.ts
+++ VERBUM_FE_SEP490/composables/useJobs.ts
@@ -2,11 +2,19 @@ import type { Job } from '@/types/job'
 export const useJobs = () => {
   const jobs = ref<Job[]>([])
   const job = ref<Job | null>(null)
-
+  const user = useAuthStore().user
+  const role = useAuthStore().user?.role
+  
   const getJobs = async () => {
     try {
-      const { data } = await useAPI('/job/get-all')
-      jobs.value = data.value as Job[]
+      if (role === 'LINGUIST') {
+      const { data } = await useAPI(`/job/get-all?$expand=assigneeNames&$filter=assigneeNames/any(a: cast(a/id, Edm.String) eq '${user?.user_id}')`)
+        jobs.value = data.value as Job[]
+      }
+      else {
+        const { data } = await useAPI('/job/get-all')
+        jobs.value = data.value as Job[]
+      }
     } catch (error) {
       console.error('Failed to fetch jobs:', error)
     }
