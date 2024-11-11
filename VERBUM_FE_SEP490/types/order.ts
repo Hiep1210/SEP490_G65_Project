acc5152df88aed95ff1d2011357ad37c950ba5fa
DnMakyr <107171443+NguyenLDHE176088@users.noreply.github.com>
2024-11-11 23:23:01 +0700

    chore: change interface for Order

--- VERBUM_FE_SEP490/types/order.ts
+++ VERBUM_FE_SEP490/types/order.ts
@@ -11,7 +11,7 @@ export interface Order {
   hasTranslateService?: boolean
   hasEditService?: boolean
   hasEvaluateService?: boolean
-  reference?: string
+  orderNote?: string
   translationFileUrls?: string[]
   referenceFileUrls?: string[]
   deliverableFileUrls?: string[]
