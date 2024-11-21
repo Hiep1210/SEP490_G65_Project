4ecd9b26d2ed8447d638457be35e4bffd28553fd
DnMakyr <107171443+NguyenLDHE176088@users.noreply.github.com>
2024-11-21 20:04:05 +0700

    chore: make seperate components for status columns

--- VERBUM_FE_SEP490/components/Works/StatusColumn.vue
+++ VERBUM_FE_SEP490/components/Works/StatusColumn.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 import { ref } from 'vue'
-import type { Work } from '~/composables/useWorks'
+import type { Job } from '~/types/job'
 
 // Define the type for the status object
 interface Status {
@@ -11,30 +11,30 @@ interface Status {
 }
 
 const props = defineProps<{
-  data: Work[]
+  data: Job[]
 }>()
 
 const allStatus = ref<Status[]>([
   {
-    status: 'ACCEPTED',
+    status: 'NEW',
     class: 'bg-cyan-200 text-black',
     currentPage: 1,
     itemsPerPage: 5 // Set the number of items to show per page for each status
   },
   {
-    status: 'IN-PROGRESS',
+    status: 'IN_PROGRESS',
     class: 'bg-teal-200 text-black',
     currentPage: 1,
     itemsPerPage: 5
   },
   {
-    status: 'COMPLETED',
+    status: 'SUBMITTED',
     class: 'bg-emerald-200 text-black',
     currentPage: 1,
     itemsPerPage: 5
   },
   {
-    status: 'CANCEL',
+    status: 'APPROVED',
     class: 'bg-gray-200 text-black',
     currentPage: 1,
     itemsPerPage: 5
@@ -42,30 +42,30 @@ const allStatus = ref<Status[]>([
 ])
 
 // Search query for filtering works by name
-const searchQuery = ref('')
+const searchQuery = ref<string>('')
 
 // Function to calculate total pages for a specific status
 const totalPages = (status: Status) => {
-  const filteredWorks = filteredWorksByStatus(status)
-  return Math.ceil(filteredWorks.length / status.itemsPerPage)
+  const filteredJobs = filteredJobsByStatus(status)
+  return Math.ceil(filteredJobs.length / status.itemsPerPage)
 }
 
 // Function to filter works based on status and search query
-const filteredWorksByStatus = (status: Status) => {
-  return props.data.filter(work => {
+const filteredJobsByStatus = (status: Status) => {
+  return props.data.filter(job => {
     const matchesStatus =
-      work.orderStatus === status.status ||
-      (status.status === 'NEW' && work.orderStatus === null)
-    const matchesQuery = work.workName.toLowerCase().includes(searchQuery.value.toLowerCase())
+      job.status === status.status ||
+      (status.status === 'NEW' && job.status === null)
+    const matchesQuery = job.name && job.name.toLowerCase().includes(searchQuery.value.toLowerCase())
     return matchesStatus && matchesQuery
   })
 }
 
 // Function to get paginated works for a specific status
-const paginatedWorks = (status: Status) => {
-  const filteredWorks = filteredWorksByStatus(status)
+const paginatedJobs = (status: Status) => {
+  const filteredJobs = filteredJobsByStatus(status)
   const start = (status.currentPage - 1) * status.itemsPerPage
-  return filteredWorks.slice(start, start + status.itemsPerPage)
+  return filteredJobs.slice(start, start + status.itemsPerPage)
 }
 
 // Navigation functions for changing pages
@@ -80,6 +80,7 @@ const previousPage = (status: Status) => {
     status.currentPage--
   }
 }
+const role = useAuthStore().user?.role as string | undefined
 </script>
 
 <template>
@@ -89,46 +90,46 @@ const previousPage = (status: Status) => {
       v-model="searchQuery"
       type="search"
       class="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-xl bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-      placeholder="Enter work name ..."
-    >
+      placeholder="Enter job name ...">
     <button
       type="submit"
-      class="text-white absolute end-2.5 bottom-2.5 bg-cyan-600 hover:bg-cyan-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-    >
+      class="text-white absolute end-2.5 bottom-2.5 bg-cyan-600 hover:bg-cyan-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
       Search
     </button>
   </div>
   <div class="flex flex-row gap-3 mt-2">
-    <div
-      v-for="item in allStatus"
-      :key="item.status"
-      class="flex flex-col w-1/4 statusCol"
-    >
-      <div :class="item.class" class="p-2 rounded-xl h-5/6">
-        <p class="font-bold text-cyan-950 text-center my-3">
-          {{ item.status }}
-        </p>
-        <div v-for="work in paginatedWorks(item)" :key="work.workId">
-          <WorksCard
-            v-if="
-              work.orderStatus === item.status ||
-              (item.status === 'NEW' && work.orderStatus === null)
-            "
-            :data="work"
-          />
+    <div v-for="item in allStatus" :key="item.status" class="flex flex-col w-1/4 statusCol">
+      <p class="font-bold text-cyan-950 text-center mb-3">
+        {{ item.status }}
+      </p>
+      <div :class="item.class" class="p-2 rounded-xl h-5/6 overflow-y-auto">
+        <div v-for="job in paginatedJobs(item)" :key="job.id">
+          <JobsDialog :job="job" :role="role">
+            <JobsCard
+              v-if="job.status === item.status || (item.status === 'NEW' && job.status === null)"
+              :data="job"
+            />
+          </JobsDialog>
         </div>
       </div>
-      <div class="flex justify-between mt-4">
-        <button :disabled="item.currentPage === 1" @click="previousPage(item)">
-          Previous
-        </button>
-        <span>Page {{ item.currentPage }} of {{ totalPages(item) }}</span>
-        <button
-          :disabled="item.currentPage === totalPages(item)"
-          @click="nextPage(item)"
-        >
-          Next
-        </button>
+      <div class="flex justify-center">
+        <div class="flex space-x-3 mt-4">
+          <Button
+            variant="outline"
+            :disabled="item.currentPage === 1 || totalPages(item) === 0"
+            @click="previousPage(item)"
+          >
+            Previous
+          </Button>
+          <span class="text-center py-2 px-1">Page {{ item.currentPage }} of {{ totalPages(item) }}</span>
+          <Button
+            variant="outline"
+            :disabled="item.currentPage === totalPages(item) || totalPages(item) === 0"
+            @click="nextPage(item)"
+          >
+            Next
+          </Button>
+        </div>
       </div>
     </div>
   </div>
