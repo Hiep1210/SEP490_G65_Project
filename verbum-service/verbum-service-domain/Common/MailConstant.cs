5f6b6f894a54dbc1ffc781daf5aa201beb150876
Hiep1210 <nguyenhoanghiep1210003@gmail.com>
2024-11-24 11:50:42 +0700

    add mail template

--- verbum-service/verbum-service-domain/Common/MailConstant.cs
+++ verbum-service/verbum-service-domain/Common/MailConstant.cs
@@ -2,8 +2,15 @@
 {
     public static class MailConstant
     {
-        public const string SUBJECT = "Hello {0}!, please confirm your email!!";
+        public const string CONFIRM_EMAIL_HEADER = "Hello {0}!, please confirm your email!!";
         public const int MailExpirationTime = 1; //hour
-
+        public const string REJECT_ORDER_HEADER = "Your order {0} has been rejected";
+        public const string ACCEPT_ORDER_HEADER = "Your order {0} has been accepted";
+        public const string COMPLETE_ORDER_HEADER = "Your order {0} has been completed";
+        public const string REJECT_JOB_HEADER = "Your job has been rejected";
+        public const string ACCEPT_JOB_HEADER = "Your job has been accepted";
+        public const string REJECT_ISSUE_HEADER = "Your issue {0} has been rejected";
+        public const string ACCEPT_ISSUE_HEADER = "Your issue {0} has been accepted";
+        public const string CANCEL_ISSUE_HEADER = "Your issue {0} has been cancelled";
     }
 }
