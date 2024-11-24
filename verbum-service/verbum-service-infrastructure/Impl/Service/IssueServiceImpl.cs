b4de15ad185ae7466a2b107de3a23f5f8588a91c
Hiep1210 <nguyenhoanghiep1210003@gmail.com>
2024-11-24 17:53:27 +0700

    fix issue

--- verbum-service/verbum-service-infrastructure/Impl/Service/IssueServiceImpl.cs
+++ verbum-service/verbum-service-infrastructure/Impl/Service/IssueServiceImpl.cs
@@ -128,7 +128,7 @@ namespace verbum_service_infrastructure.Impl.Service
         {
             string status = await context.Issues.Where(x => x.IssueId == request.IssueId).Select(x => x.Status).FirstOrDefaultAsync();
             if (status != IssueStatusEnum.CANCEL.ToString()) throw new BusinessException(AlertMessage.Alert(ValidationAlertCode.INVALID, "issue status"));
-            if (await context.Issues.AnyAsync(x => x.IssueName.Equals(request.IssueName))) throw new BusinessException(AlertMessage.Alert(ValidationAlertCode.DUPLICATE, "issue name"));
+            if (await context.Issues.AnyAsync(x => x.IssueName.Equals(request.IssueName) && !x.IssueId.Equals(request.IssueId))) throw new BusinessException(AlertMessage.Alert(ValidationAlertCode.DUPLICATE, "issue name"));
 
             Issue updateIssue = mapper.Map<Issue>(request);
             updateIssue.UpdatedAt = DateTime.Now;
