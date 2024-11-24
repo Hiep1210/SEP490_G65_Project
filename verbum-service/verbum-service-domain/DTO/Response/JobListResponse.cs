68354b95a5f5ac343daa2f58999e489f514e6775
chicken271 <nguyenminhtuan271203@gmail.com>
2024-11-24 22:23:42 +0700

    fix job list response

--- verbum-service/verbum-service-domain/DTO/Response/JobListResponse.cs
+++ verbum-service/verbum-service-domain/DTO/Response/JobListResponse.cs
@@ -9,6 +9,7 @@ namespace verbum_service_domain.DTO.Response
     public class JobListResponse
     {
         public Guid Id { get; set; }
+        public Guid WorkId { get; set; }
 
         public string Name { get; set; } = null!;
 
