1f3eeec3aec194f1438edcbd3e4566ba8c202a9f
Hiep1210 <nguyenhoanghiep1210003@gmail.com>
2024-11-25 23:00:41 +0700

    fix job name

--- verbum-service/verbum-service-infrastructure/Impl/Service/JobServiceImpl.cs
+++ verbum-service/verbum-service-infrastructure/Impl/Service/JobServiceImpl.cs
@@ -37,7 +37,7 @@ namespace verbum_service_infrastructure.Impl.Service
                                 Job job = new Job
                                 {
                                     Id = Guid.NewGuid(),
-                                    Name = targetLangId + "_" + work.ServiceCodeNavigation.ServiceName + "_" + docUrl.Split("/")[^1].Split(".docx")[0].Replace("%20", " "),
+                                    Name = targetLangId + "_" + work.ServiceCodeNavigation.ServiceName + "_" + docUrl.Split("/")[^1].Split(".")[0],
                                     Status = JobStatus.NEW.ToString(),
                                     CreatedAt = DateTime.Now,
                                     UpdatedAt = DateTime.Now,
