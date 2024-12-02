d6b532d87add4ca8c2b0c8e727eb3b6464509b49
Lâm Phùng <91207458+laamwwibu@users.noreply.github.com>
2024-12-02 09:55:47 +0000

    styling table and work details

--- VERBUM_FE_SEP490/pages/(tms)/works/details/[id].vue
+++ VERBUM_FE_SEP490/pages/(tms)/works/details/[id].vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
-import { getWorkBadgeClass } from '~/utils/getBadgeClass';
+import { languages } from '~/constants/languages'
+import { getWorkBadgeClass } from '~/utils/getBadgeClass'
 
 const { isLoading, work, getWorkById } = useWorks()
 const { jobs, getJobsOfWork } = useJobs()
@@ -8,30 +9,52 @@ const role = useAuthStore().user?.role as string | undefined
 const route = useRoute()
 const workId = route.params.id as string
 onMounted(async () => {
-    await getJobsOfWork(workId)
-    await getWorkById(workId)
-    if (role?.includes('MANAGER')) {
-        await getAssignList()
-    }
+  await getJobsOfWork(workId)
+  await getWorkById(workId)
+  if (role?.includes('MANAGER')) {
+    await getAssignList()
+  }
 })
 
 provide('assignList', assignList)
 </script>
 
 <template>
-    <LoadingSpinner v-if="isLoading"/>
-    <div v-else>
-        <div v-if="work" class="mb-4">
-            <h1 class="text-4xl font-bold mb-4">Works Details</h1>
-            <div class="flex space-x-4">
-                <div class="space-y-2">
-                    <p class="text-3xl text-primary font-semibold">{{ work?.workName }}</p>
-                    <Badge :class="getWorkBadgeClass(work?.orderStatus)">{{ work?.orderStatus }}</Badge>
-                    <span class="text-3xl font-semibold flex space-x-1"><p>Due date:</p><p class="font-normal">{{ work?.dueDate }}</p>
-                    </span>
-                </div>
-            </div>
+  <LoadingSpinner v-if="isLoading" />
+  <div v-else class="h-full overflow-y-hidden">
+    <div v-if="work" class="flex flex-col mb-4 h-full">
+      <div class="flex space-x-4">
+        <div class="space-y-2">
+          <p class="text-[2rem] font-bold text-primary underline">
+            {{ work?.workName }}
+          </p>
+          <Badge :class="getWorkBadgeClass(work?.orderStatus)">{{
+            work?.orderStatus
+          }}</Badge>
+          <h1 class="font-semibold">
+            Due Date:
+            <span class="font-bold hyper-link">{{
+              formatToVietnamTimezone(work?.dueDate)
+            }}</span>
+          </h1>
+          <div class="flex gap-2">
+            <Badge variant="default">{{
+              languages.find(
+                (language) => language.languageId === work?.sourceLanguageId
+              )?.languageName
+            }}</Badge>
+            <LucideArrowBigRight />
+            <Badge
+              v-for="lang in work?.targetLanguageId || []"
+              :key="lang"
+              variant="secondary"
+            >
+              {{ getLanguageName(lang) }}
+            </Badge>
+          </div>
         </div>
-        <WorksStatusColumn :data="jobs" />
+      </div>
+      <WorksStatusColumn :data="jobs" />
     </div>
+  </div>
 </template>
