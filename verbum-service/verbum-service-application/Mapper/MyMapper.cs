72ab18b159f2bf5c1cebc2f705cfdcb2dab17594
chicken271 <nguyenminhtuan271203@gmail.com>
2024-12-02 23:21:13 +0700

    fix discount and order details

--- verbum-service/verbum-service-application/Mapper/MyMapper.cs
+++ verbum-service/verbum-service-application/Mapper/MyMapper.cs
@@ -50,7 +50,9 @@ namespace verbum_service_application.Mapper
                     ServiceOrder = work.ServiceCodeNavigation.ServiceOrder,
                 }))))
                 .ForMember(dest => dest.DeleteddFileUrls, opt => opt.MapFrom(src => src.OrderReferences.Where(t => t.IsDeleted == true).Select(t => t.ReferenceFileUrl).ToList()))
-                .ForMember(dest => dest.CompletedDate, opt => opt.MapFrom(src => src.Receipts.Where(t => t.DepositeOrPayment == false).Select(t => t.PayDate).FirstOrDefault()));
+                .ForMember(dest => dest.CompletedDate, opt => opt.MapFrom(src => src.Receipts.Where(t => t.DepositeOrPayment == false).Select(t => t.PayDate).FirstOrDefault()))
+                .ForMember(dest => dest.DiscountName, opt => opt.MapFrom(src => src.Discount.DiscountName))
+                .ForMember(dest => dest.DiscountAmount, opt => opt.MapFrom(src => src.Discount.DiscountPercent));
             CreateMap<OrderReference, UploadOrderFileRequest>().ReverseMap();
             CreateMap<Work, WorkResponse>()
                 .ForMember(dest => dest.SourceLanguageId, opt => opt.MapFrom(src => src.Order.SourceLanguageId))
