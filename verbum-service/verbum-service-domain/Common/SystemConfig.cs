de060852c5b99ec835428086818dd2e3b85a1d54
Nguyễn Hoàng Hiệp <nguyenhoanghiep1210003@gmail.com>
2024-12-02 22:48:10 +0700

    update host

--- verbum-service/verbum-service-domain/Common/SystemConfig.cs
+++ verbum-service/verbum-service-domain/Common/SystemConfig.cs
@@ -9,7 +9,7 @@ namespace verbum_service_domain.Common
     public static class SystemConfig
     {
         //public const string CONNECTION_STRING = "Host=localhost;Database=abc;Username=postgres;Password=123";
-        //public const string CONNECTION_STRING = "Host=localhost;Database=verbum_db;Username=postgres;Password=123456";
+        //public const string CONNECTION_STRING = "Host=host.docker.internal;Database=verbum_db;Username=postgres;Password=123456";
         public const string CONNECTION_STRING = "Host=verbum-verbum.h.aivencloud.com;Database=verbum;Username=avnadmin;Password=AVNS_3gU17QnAszLI3He-LgO;Port=17672;Trust Server Certificate=true";
         public const int ACCESS_TOKEN_LIFE = 5; //hour
         public const int REFRESH_TOKEN_LIFE = 14; //days
