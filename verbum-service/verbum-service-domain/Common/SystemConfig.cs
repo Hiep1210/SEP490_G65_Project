02643c99a2993028ba6df5ee09560e74f70f9324
Lâm Phùng <91207458+laamwwibu@users.noreply.github.com>
2024-12-16 16:31:36 +0700

    Update SystemConfig.cs

--- verbum-service/verbum-service-domain/Common/SystemConfig.cs
+++ verbum-service/verbum-service-domain/Common/SystemConfig.cs
@@ -16,6 +16,6 @@ namespace verbum_service_domain.Common
         public const string DOMAIN = "http://localhost:8000/api/auth";
         public const string BE_DOMAIN = "http://localhost:8000/api";
         // public const string FE_DOMAIN = "http://localhost:3000";
-        public const string FE_DOMAIN = "https://kcl2yz-3000.csb.app";
+        public const string FE_DOMAIN = "https://6ghcff-3000.csb.app";
     }
 }
